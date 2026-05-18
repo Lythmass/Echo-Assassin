d5985627eda5263d906d4db73c6d161f552b9b97
Lythmass <lythmass17@gmail.com>
2026-05-18 17:40:51 +0400

    feature: add responsive player controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,27 +3,58 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("Player Configs")]
     [SerializeField]
     float speed;
 
     [SerializeField]
     float jumpForce;
 
+    [Header("Mechanic Configs")]
+    [SerializeField]
+    float fallGravityMultiplier;
+
+    [SerializeField]
+    float jumpGravityMultiplier;
+
+    [SerializeField]
+    float coyoteTime;
+    float currentCoyoteTime;
+
+    [Header("Ground Detection")]
+    [SerializeField]
+    Transform groundDetector;
+
+    [SerializeField]
+    float radius;
+    bool isGrounded;
     InputAction moveAction;
     InputAction jumpAction;
     Vector2 moveInput;
     Rigidbody2D rb;
+    LayerMask platformLayer;
 
     void Awake()
     {
         moveAction = InputSystem.actions.FindAction("Move");
         jumpAction = InputSystem.actions.FindAction("Jump");
+        platformLayer = LayerMask.GetMask("Platform");
         rb = GetComponent<Rigidbody2D>();
+        currentCoyoteTime = coyoteTime;
     }
 
     void Update()
     {
         moveInput = moveAction.ReadValue<Vector2>();
+        isGrounded = Physics2D.OverlapCircle(groundDetector.position, radius, platformLayer);
+        if (!isGrounded)
+        {
+            currentCoyoteTime -= Time.deltaTime;
+        }
+        else
+        {
+            currentCoyoteTime = coyoteTime;
+        }
     }
 
     void FixedUpdate()
@@ -34,9 +65,30 @@ public class PlayerController : MonoBehaviour
 
     void Jump()
     {
-        if (jumpAction.IsPressed())
+        if (jumpAction.IsPressed() && currentCoyoteTime > Mathf.Epsilon)
         {
             rb.linearVelocityY = jumpForce;
+            currentCoyoteTime = 0f;
+            isGrounded = false;
         }
+
+        if (rb.linearVelocityY < 0)
+        {
+            rb.linearVelocityY +=
+                (fallGravityMultiplier - 1) * Physics2D.gravity.y * Time.fixedDeltaTime;
+        }
+        else if (rb.linearVelocityY > 0 && !jumpAction.IsPressed())
+        {
+            rb.linearVelocityY +=
+                (jumpGravityMultiplier - 1) * Physics2D.gravity.y * Time.fixedDeltaTime;
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        if (groundDetector == null)
+            return;
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(groundDetector.position, radius);
     }
 }
